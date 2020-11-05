9b78aa873d6a81ead800673e35202936b09100d2
Hubert <hubert.kniola@student.put.poznan.pl>
2020-11-05 22:27:44 +0100

    final

--- CipherModes/src/MainClass.java
+++ CipherModes/src/MainClass.java
@@ -16,36 +16,36 @@ public class MainClass {
         String mode = "ECB", outputName = "resultECB", fileName="200MBfile";
         System.out.println("[ECB] Encrypted: ");
         var encryptedMessage= InputOutput.encMode(mode, outputName, fileName);
-        //System.out.println("[ECB] Decrypted: ");
-        //InputOutput.decMode(mode, encryptedMessage);
+        System.out.println("[ECB] Decrypted: ");
+        InputOutput.decMode(mode, encryptedMessage);
 
         mode = "CBC";
         outputName = "resultCBC";
-        //System.out.println("[CBC] Encrypted: ");
+        System.out.println("[CBC] Encrypted: ");
         encryptedMessage = InputOutput.encMode(mode, outputName, fileName);
-        //System.out.println("[CBC] Decrypted: ");
-        //InputOutput.decMode(mode, encryptedMessage);
+        System.out.println("[CBC] Decrypted: ");
+        InputOutput.decMode(mode, encryptedMessage);
 
         mode = "OFB";
         outputName = "resultOFB";
-        //System.out.println("[OFB] Encrypted: ");
+        System.out.println("[OFB] Encrypted: ");
         encryptedMessage = InputOutput.encMode(mode, outputName, fileName);
-        //System.out.println("[OFB] Decrypted: ");
-        //InputOutput.decMode(mode, encryptedMessage);
+        System.out.println("[OFB] Decrypted: ");
+        InputOutput.decMode(mode, encryptedMessage);
 
         mode = "CFB";
         outputName = "resultCFB";
-        //System.out.println("[CFB] Encrypted: ");
+        System.out.println("[CFB] Encrypted: ");
         encryptedMessage = InputOutput.encMode(mode, outputName, fileName);
-        //System.out.println("[CFB] Decrypted: ");
-        //InputOutput.decMode(mode, encryptedMessage);
+        System.out.println("[CFB] Decrypted: ");
+        InputOutput.decMode(mode, encryptedMessage);
 
         mode = "CTR";
         outputName = "resultCTR";
-        //System.out.println("[CTR] Encrypted: ");
+        System.out.println("[CTR] Encrypted: ");
         encryptedMessage = InputOutput.encMode(mode, outputName, fileName);
-        //System.out.println("[CTR] Decrypted: ");
-        //InputOutput.decMode(mode, encryptedMessage);
+        System.out.println("[CTR] Decrypted: ");
+        InputOutput.decMode(mode, encryptedMessage);
 
         //Tests.CorruptionTest();
     }
